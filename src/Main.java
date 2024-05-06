34603b5390c6948c6ace23289194793259359557
Mariluzfg <mariluz.fortea@educa.madrid.org>
2024-05-06 21:07:44 +0200

    Primera versión

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //rama secundaria
+        System.out.println("Añadiendo en rama secundaria");
     }
 }
